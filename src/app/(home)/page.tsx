794de42aaebdb2c120757c876b86e32c30faefca
Matthew <m.eric.flojo@gmail.com>
2025-03-22 14:52:44 -0700

    Add tRPC

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Matthew" });
+
   return (
-    <div className="font-bold text-rose-500">
-      I will load videos in the future
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error</p>}>
+          <PageClient />
+        </ErrorBoundary>
+        
+      </Suspense>  
+    
+    </HydrateClient>
   );
 }
