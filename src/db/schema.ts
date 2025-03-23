50d44ee540bca20c8cd7cc6d0132fcf6c85893c8
Matthew <m.eric.flojo@gmail.com>
2025-03-23 16:16:48 -0700

    Add thumbnail/gif to videos

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
 export const users = pgTable("users", {
@@ -37,6 +37,9 @@ export const videos = pgTable("videos", {
     muxPlaybackId: text("mux_playback_id").unique(),
     muxTrackId: text("mux_track_id").unique(),
     muxTrackStatus: text("mux_track_status"),
+    thumbnailUrl: text("thumbnail_url"),
+    previewUrl: text("preview_url"),
+    duration: integer("duration"),
     userId: uuid("user_id").references(() => users.id, {
         onDelete: "cascade",
     }).notNull(),
