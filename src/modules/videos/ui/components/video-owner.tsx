90b9e6a2490010a0a11c918b978bd13a82278b7a
Matthew <m.eric.flojo@gmail.com>
2025-03-28 20:24:46 -0700

    Add subscriptions/unsub to videos

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
 import { UserInfo } from "@/modules/users/ui/components/user-info";
+import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription";
 
 interface VideoOwnerProps {
     user: VideoGetOneOutput["user"];
@@ -12,7 +13,12 @@ interface VideoOwnerProps {
 };
 
 export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
-    const { userId } = useAuth();
+    const { userId: clerkUserId, isLoaded } = useAuth();
+    const { isPending, onClick } = useSubscription({
+        userId: user.id,
+        isSubscribed: user.viewerSubscribed,
+        fromVideoId: videoId
+    })
 
     return (
         <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
@@ -22,13 +28,12 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
                     <div className="flex flex-col gap-1 min-w-0">
                         <UserInfo size="lg" name={user.name} />
                         <span className="text-sm text-muted-foreground line-clamp-1">
-                            {/**Properly full sub count */}
-                            {0} subscribers
+                            {user.subscriberCount} subscribers
                         </span>
                     </div>
                 </div>
             </Link>
-            {userId === user.clerkId ? (
+            {clerkUserId === user.clerkId ? (
                 <Button
                     className="rounded-full"
                     asChild
@@ -40,9 +45,9 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
                 </Button>
             ) : (
                 <SubscriptionButton
-                    onClick={() => {}}
-                    disabled={false}
-                    isSubscribed={false}
+                    onClick={onClick}
+                    disabled={isPending || !isLoaded}
+                    isSubscribed={user.viewerSubscribed}
                     className="flex-none"
                 />
             )}
