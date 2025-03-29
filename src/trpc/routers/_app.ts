aace240a3aa52fc447f6f76b3e3c96b08e8827fd
Matthew <m.eric.flojo@gmail.com>
2025-03-28 17:21:59 -0700

    Add video reactions (like/dislike)

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,16 @@
 import { categoriesRouter } from '@/modules/categories/server/procedures';
-
 import { createTRPCRouter} from '../init';
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { videosRouter } from '@/modules/videos/server/procedures';
 import { VideoViewsRouter } from '@/modules/video-views/server/procedures';
+import { VideoReactionsRouter } from '@/modules/video-reactions/server/procedures';
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
   videos: videosRouter,
   videoViews: VideoViewsRouter,
+  videoReactions: VideoReactionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
