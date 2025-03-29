dbab981acc5ad8d4bb51616038553dab047c0294
Matthew <m.eric.flojo@gmail.com>
2025-03-29 12:26:07 -0700

    Add infinite scrolling comments/deletable comments

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_LIMIT } from "@/constants";
 import { VideoView } from "@/modules/videos/ui/views/video-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
@@ -11,14 +12,13 @@ const Page = async ({ params }: PageProps) => {
     const { videoId } = await params;
 
     void trpc.videos.getOne.prefetch({ id: videoId });
-    //TODO: dont forget to change to prefetchInfinite
-    void trpc.comments.getMany.prefetch({ videoId: videoId });
+    void trpc.comments.getMany.prefetchInfinite({ videoId, limit: DEFAULT_LIMIT });
  
     return ( 
         <HydrateClient>
             <VideoView videoId={videoId} />
         </HydrateClient>
      );
-}
+};
  
 export default Page;
--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,10 +1,33 @@
 import { db } from "@/db";
 import { comments, users } from "@/db/schema";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { eq, getTableColumns } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import { and, count, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 import { z } from "zod";
 
 export const commentsRouter = createTRPCRouter({
+    remove: protectedProcedure
+        .input(z.object({
+            id: z.string().uuid(),
+        }))
+        .mutation(async ({ input, ctx }) => {
+            const { id: userId } = ctx.user;
+            const { id } = input;
+
+            const [deletedComment] = await db
+                .delete(comments)
+                .where(and(
+                    eq(comments.userId, userId),
+                    eq(comments.id, id)
+                ))
+                .returning();
+
+            if (!deletedComment) {
+                throw new TRPCError({ code: "NOT_FOUND" });
+            }
+
+            return deletedComment;
+        }),
     create: protectedProcedure
         .input(z.object({
             videoId: z.string().uuid(),
@@ -17,7 +40,7 @@ export const commentsRouter = createTRPCRouter({
             const [createdComment] = await db
                 .insert(comments)
                 .values({ userId, videoId, value })
-                .returning()
+                .returning();
 
             return createdComment;
         }),
@@ -25,20 +48,62 @@ export const commentsRouter = createTRPCRouter({
         .input(
             z.object({
                 videoId: z.string().uuid(),
+                cursor: z.object({
+                    id: z.string().uuid(),
+                    updatedAt: z.date(),
+                }).nullish(),
+                limit: z.number().min(1).max(100),
             }),
         )
         .query(async ({ input }) => {
-            const { videoId } = input;
+            const { videoId, cursor, limit } = input;
 
-            const data = await db
-                .select({
-                    ...getTableColumns(comments),
-                    user: users,
-                })
-                .from(comments)
-                .where(eq(comments.videoId, videoId))
-                .innerJoin(users, eq(comments.userId, users.id))
+            const [totalData, data] = await Promise.all([
+                db
+                    .select({
+                        count: count(),
+                    })
+                    .from(comments)
+                    .where(eq(comments.videoId, videoId)),
+                db
+                    .select({
+                        ...getTableColumns(comments),
+                        user: users,
+                    })
+                    .from(comments)
+                    .where(and(
+                        eq(comments.videoId, videoId),
+                        cursor
+                            ? or(
+                                lt(comments.updatedAt, cursor.updatedAt),
+                                and(
+                                    eq(comments.updatedAt, cursor.updatedAt),
+                                    lt(comments.id, cursor.id)
+                                )
+                            )
+                            : undefined,
+                    ))
+                    .innerJoin(users, eq(comments.userId, users.id))
+                    .orderBy(desc(comments.updatedAt), desc(comments.id))
+                    .limit(limit + 1)
+            ])
 
-            return data;
+            const hasMore = data.length > limit;
+            //Remove the last item if there is more data
+            const items = hasMore ? data.slice(0, -1) : data;
+            //Set the next cursor to the last item if there is more data
+            const lastItem = items[items.length - 1];
+            const nextCursor = hasMore 
+                ? {
+                    id: lastItem.id,
+                    updatedAt: lastItem.updatedAt,
+                }
+                : null;
+
+            return {
+                totalCount: totalData[0].count,
+                items,
+                nextCursor,
+            };
         }),
 });
