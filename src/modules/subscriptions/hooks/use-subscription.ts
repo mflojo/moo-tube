7fd8a65876f41f4118aec5b13f083a5bdcc528e5
Matthew <m.eric.flojo@gmail.com>
2025-04-05 13:01:26 -0700

    Add all subscription list page

--- src/modules/subscriptions/hooks/use-subscription.ts
+++ src/modules/subscriptions/hooks/use-subscription.ts
@@ -19,9 +19,10 @@ export const useSubscription = ({
     const subscribe = trpc.subscriptions.create.useMutation({
         onSuccess: () => {
             toast.success("Subscribed");
+            utils.subscriptions.getMany.invalidate();
             utils.videos.getManySubscribed.invalidate();
             utils.users.getOne.invalidate({ id: userId });
-            //TODO: reinvalidate subscriptions.getMany
+      
             if (fromVideoId) {
                 utils.videos.getOne.invalidate({ id: fromVideoId });
             }
@@ -38,10 +39,9 @@ export const useSubscription = ({
     const unsubscribe = trpc.subscriptions.remove.useMutation({
         onSuccess: () => {
             toast.success("Unsubscribed");
-            //TODO: reinvalidate subscriptions.getMany
+            utils.subscriptions.getMany.invalidate();
             utils.videos.getManySubscribed.invalidate();
             utils.users.getOne.invalidate({ id: userId });
-
 
             if (fromVideoId) {
                 utils.videos.getOne.invalidate({ id: fromVideoId });
